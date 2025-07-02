45e341ae56feac6fb7c86687a7e178157082118a
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-07-01 23:34:21 -0700

    feat: update User component to enhance avatar display and user information

--- Frontend/RentChain/src/components/ui/User.jsx
+++ Frontend/RentChain/src/components/ui/User.jsx
@@ -13,8 +13,8 @@ export default function User() {
       {open && (
         <div className="absolute right-0 z-50 mt-2 w-[30rem] origin-top-right bg-white border border-gray-200 shadow-xl rounded-xl">
           <div className="flex flex-col items-center p-4 border-b border-gray-100">
-            <FaUserCircle className="w-24 h-24 mb-2 text-gray-400" />
-            <button className="text-2xl text-blue-600 hover:underline">Change Avatar</button>
+            <FaUserCircle className="w-24 h-24 mb-4 text-gray-400" />
+            <p className="text-3xl font-medium text-secondary">John Doe</p>
           </div>
           <ul className="py-2 pb-4 pl-4 space-y-4 text-2xl text-gray-700">
             <li className="px-4 py-4 cursor-pointer hover:bg-gray-100">Change Name</li>
