2212adfc0864a714a36433268c729259fa05d733
Jo-shua09 <joshuaonyeka039@gmail.com>
2025-07-04 20:57:45 -0700

    fix: adjust layout and text alignment in ConnectWallet component

--- Frontend/RentChain/src/pages/ConnectWallet.jsx
+++ Frontend/RentChain/src/pages/ConnectWallet.jsx
@@ -6,13 +6,15 @@ export default function ConnectWallet() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex items-center justify-center w-full h-screen max-h-screen bg-gray-50">
+    <div className="flex items-center justify-center w-full h-screen max-h-screen section-page bg-gray-50">
       <div className="flex flex-col items-center w-full">
         <div className="mb-5 cursor-pointer w-fit">
           <img src="/logo.png" alt="logo image" loading="lazy" className="w-[20rem] h-fit cursor-pointer" />
         </div>
-        <h3 className="text-5xl font-semibold normal-case pb-7 text-secondary">Decentralized Rental Management Platform</h3>
-        <p className="text-[1.7rem] font-medium normal-case text-secondary">Connect your wallet to access the blockchain-powered rental ecosystem</p>
+        <h3 className="text-5xl font-semibold text-center normal-case pb-7 text-secondary">Decentralized Rental Management Platform</h3>
+        <p className="text-[1.7rem] font-medium normal-case text-center text-secondary">
+          Connect your wallet to access the blockchain-powered rental ecosystem
+        </p>
         <div className="flex flex-col items-center justify-center w-full max-w-5xl px-10 py-10 mt-8 bg-white shadow-sm rounded-xl">
           <div className="flex items-center mb-5 gap-x-5">
             <IoWalletOutline className="text-6xl text-primary" />
